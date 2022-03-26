37dcb341b3f5fe7bff6f83d0288eedc4bc4ffe24
razrobill <tb1126@york.ac.uk>
2022-03-26 15:59:54 +0000

    changed the tables to better reflect user stories, and functionality of a library

--- exercise18 (1).sql
+++ exercise18 (1).sql
@@ -9,16 +9,17 @@ create table LibraryUsers(
         USER_ID int not null,
         FIRST_NAME varchar(50) not null,
         LAST_NAME varchar(50) not null, 
-        EMAIL varchar(100) not null,
+        CONTACT_DETAILS int not null,
         D_O_B date not null,
         PREFERENCES int not null,
-        PURCHASES int not null,        
-        foreign key(PURCHASES) references Purchases(PRODUCT_NO),
-        foreign key(PREFERENCES) references Genres(ID))
+        LOANS int not null,        
+        foreign key(LOANS) references Loans(PRODUCT_NO),
+        foreign key(PREFERENCES) references Genres(ID),
+        foreign key(CONTACT_DETAILS) references Addresses(ID))
         engine = innodb;
 
 #create table for purchases
-create table Purchases(
+create table Loans(
 		ID int not null,
         ORDER_ID int not null,
         PRODUCT_NO int not null primary key auto_increment);
@@ -30,11 +31,28 @@ create table Genres(
 		ID int not null primary key auto_increment,
         GENRE_NAME varchar(50) not null,
         DESCRIPTION_ text not null);
+        
+#create table for mailing address
+create table Addresses(
+		ID int not null primary key,
+        EMAIL varchar(50) not null,
+        ADDRESS_LINE_1 varchar(50) not null,
+        ADDRESS_LINE_2 varchar(50) not null,
+        CITY varchar(50) not null,
+        PHONE_NO int not null);
+
+
+
+
 	
 
 #testing functionality
 
 insert into LibraryUsers(FIRST_NAME, LAST_NAME) values ('talia', 'blain');
+
+#DROP database Exercise18;
+
+
 
 
         
